9f68907210e73d95b29a7a1ed1e5407379ba4a66
Муса <good.24165@gmail.com>
2026-02-26 17:08:48 +0300

    Add analyst, doctor and manager pages

--- DoctorMomFrontend/MainWindow.xaml.cs
+++ DoctorMomFrontend/MainWindow.xaml.cs
@@ -1,13 +1,4 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace DoctorMomFrontend
 {
@@ -20,7 +11,7 @@ namespace DoctorMomFrontend
         {
             InitializeComponent();
 
-            MainFrame.Navigate(new RegistrationPage());
+            MainFrame.Navigate(new ManagerMainPage());
         }
     }
 }
